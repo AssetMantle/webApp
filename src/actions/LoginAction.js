1bee32f6b1f7bfee3c521c3e01ee6b95091da834
blackpanther8132 <blackpanther8132@gmail.com>
2020-10-28 18:16:09 +0530

    added create random wallet

--- src/actions/LoginAction.js
+++ src/actions/LoginAction.js
@@ -1,11 +1,14 @@
-import React from "react";
-import {Form, Button} from "react-bootstrap";
+import React, {useState} from "react";
+import {Modal, Form, Button} from "react-bootstrap";
 import {useHistory} from "react-router-dom";
 import keyUtils from "persistenceJS/utilities/keys";
 
 const LoginAction = () => {
     const history = useHistory();
-
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const [errorUsername , setErrorUsername] = useState("");
+    const [accountData, setAccountData] = useState({});
     const handleSubmit = async event => {
         const error = keyUtils.createWallet(event.target.mnemonic.value)
         if (error.error != null) {
@@ -17,7 +20,12 @@ const LoginAction = () => {
         localStorage.setItem("mnemonic", event.target.mnemonic.value)
         history.push('/IdentityList');
     }
-
+const handleCreateWallet = () =>{
+    setShow(true);
+    const walletInfo = keyUtils.createRandomWallet();
+    setAccountData(walletInfo)
+    console.log(walletInfo, "walletinfo")
+}
     return (
         <div className="container">
             <div className="accountInfo">
@@ -38,11 +46,42 @@ const LoginAction = () => {
                             >
                                 Submit
                             </Button>
+                            <p className="mrt-10">New User ?</p>
+                            <Button
+                                variant="primary"
+                               onClick = {handleCreateWallet}
+                            >
+                                Sign Up
+                            </Button>
                             </div>
                         </Form>
                     </div>
                 </div>
             </div>
+            <Modal show={show} onHide={handleClose} className="accountInfoModel" centered>
+            <Modal.Header>
+                <div className="icon success-icon">
+                    <i className="mdi mdi-check"></i>
+                </div>
+            </Modal.Header>
+            <Modal.Body>   
+                    <div>
+                        <div className="content">
+                        <p><b>Address: </b> {`${accountData.address}`}</p>
+                        <p><b>Mnemonic:</b>  {`${accountData.mnemonic}`}</p>
+                        <p className="note">Note: Welcome
+                          Your account have been created.
+                          Please save above details for future use:</p>
+                          </div>
+                    </div>
+                    
+              </Modal.Body>
+            <Modal.Footer>
+              <Button variant="primary" onClick={handleClose}>
+                ok
+              </Button>
+            </Modal.Footer>
+          </Modal>
         </div>
     );
 }
