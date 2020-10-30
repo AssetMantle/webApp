449a7dbac2148a5913148423267d2b65f179f722
puneet2019 <puneet@persistence.one>
2020-10-30 14:37:14 +0530

    reformatting code.

--- src/actions/IdentityActions/Tabs.js
+++ src/actions/IdentityActions/Tabs.js
@@ -1,35 +1,36 @@
-import React, { useState } from "react";
-import { Tabs, Tab } from "react-bootstrap";
+import React, {useState} from "react";
+import {Tabs, Tab} from "react-bootstrap";
 import Orders from "./Orders";
 import MutateAsset from "./Maintainer";
 import Assets from "./Assets";
 import Idenities from "../IdentityList";
+
 const ActionsSwitcher = () => {
-  const [key, setKey] = useState("home");
-  return (
-    <div className="container">
-      <Tabs
-        id="controlled-tab-example"
-        activeKey={key}
-        onSelect={(k) => setKey(k)}
-        className="assetTabs Tabs"
-      >
-      
-        <Tab eventKey="home" title="Assets">
-          <Assets />
-        </Tab>
-        <Tab eventKey="Idenities" title="Idenities">
-          <Idenities />
-        </Tab>
-        <Tab eventKey="profile" title="Orders">
-          <MutateAsset />
-        </Tab>
-        <Tab eventKey="contact" title="Maintainer">
-          <Orders />
-        </Tab>
-      </Tabs>
-    </div>
-  );
+    const [key, setKey] = useState("home");
+    return (
+        <div className="container">
+            <Tabs
+                id="controlled-tab-example"
+                activeKey={key}
+                onSelect={(k) => setKey(k)}
+                className="assetTabs Tabs"
+            >
+
+                <Tab eventKey="home" title="Assets">
+                    <Assets/>
+                </Tab>
+                <Tab eventKey="Idenities" title="Idenities">
+                    <Idenities/>
+                </Tab>
+                <Tab eventKey="profile" title="Orders">
+                    <MutateAsset/>
+                </Tab>
+                <Tab eventKey="contact" title="Maintainer">
+                    <Orders/>
+                </Tab>
+            </Tabs>
+        </div>
+    );
 };
 
 export default ActionsSwitcher;
