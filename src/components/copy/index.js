447180ea4d4dd0154c20fd5df06a36b44f0e7a29
blackpanther8132 <blackpanther8132@gmail.com>
2021-03-24 11:05:33 +0530

    added copy status

--- src/components/copy/index.js
+++ src/components/copy/index.js
@@ -2,11 +2,22 @@ import {CopyToClipboard} from 'react-copy-to-clipboard';
 import React, {useState} from 'react';
 
 const Copy = (props) => {
+    const [copyValue, setCopyValue] = useState(false)
+    const onCopy = () => {
+        setCopyValue(true)
+        setTimeout(() => {
+            setCopyValue(false)
+        }, 1000);
+    };
+
     return (
         <div className="copy-section">
-            <CopyToClipboard  text={props.id}>
+            <CopyToClipboard onCopy={onCopy} text={props.id}>
                 <p>copy</p>
             </CopyToClipboard>
+            <section className="copy-result">
+                {copyValue ? <span>Copied</span> : null}
+            </section>
         </div>
     );
 };
