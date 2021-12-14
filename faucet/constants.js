b74292105f65ca7ac2703fad04e9302a4f35714e
vishrutsingh <vsinghv5t@gmail.com>
2021-12-14 16:27:06 +0530

    added constants

--- faucet/constants.js
+++ faucet/constants.js
@@ -5,6 +5,8 @@ const CHAIN_ID = "test-mantle-1";
 let FaucetList=[];
 const prefix= "mantle";
 const gas_price = "0umantle";
+const IP_WINDOW = 24 * 60 * 60 * 1000; // 24 hours
+const IP_DRIP_LIMIT = 10;
 module.exports = {
     FAUCET_LIST_LIMIT,
     AMOUNT,
@@ -13,4 +15,6 @@ module.exports = {
     FaucetList,
     prefix,
     gas_price,
+    IP_WINDOW,
+    IP_DRIP_LIMIT
 }
