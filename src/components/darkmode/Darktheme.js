c9c56239db5e5a47a78e7957d67c2c675fc4209a
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2022-06-13 16:50:46 +0600

    Rebranding the app (#7)
    * ignored lock file
    
    * 🦄assets update
    
    * 💅style update
    
    * 🛠️made dark alawys, and disabled button
    
    * 📕🛠️logo and link update
    
    * color changed

--- src/components/darkmode/Darktheme.js
+++ src/components/darkmode/Darktheme.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import {useDarkMode} from './useDarkMode';
-import {darkTheme, lightTheme} from './theme';
+import {darkTheme} from './theme';
 import {GlobalStyles} from './global';
 import {ThemeProvider} from 'styled-components';
 import Icon from "../../icons";
 
 const Darktheme = () => {
     const [theme, toggleTheme] = useDarkMode();
-    const themeMode = theme === 'light' ? lightTheme : darkTheme;
+    const themeMode = theme === 'light' ? darkTheme : darkTheme;
     return (
         <ThemeProvider theme={themeMode}>
             <GlobalStyles/>
-            <button onClick={toggleTheme} className="dark-mode-button"
+            <button onClick={toggleTheme} className="dark-mode-button" disabled={true}
                 title={theme === 'light' ? 'Switch to dark mode' : 'Switch to light mode'}>
                 <Icon
                     viewClass="icon"
